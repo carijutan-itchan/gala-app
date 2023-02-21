f5e2f64f59fc91b07eef0da1558aa7e373f7ab20
itchan <christianbaman4@gmail.com>
2023-02-21 12:20:11 +0800

    Add button shared component

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import ButtonPrimary from '../shared/buttons/ButtonPrimary';
 
 const App: React.FC = (): JSX.Element => {
   return <div>
     <h1> Jhon miss na kita Jhon  - JEPOY </h1>
-  </div>;
+
+    {/* @ts-ignore */}
+    <ButtonPrimary
+      className="mt-6 sm:mt-11"
+      buttonName='Button'
+    />
+  </div>
 };
 
 export default App;
